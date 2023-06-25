589bbed77bbe06ee8a097a9929968e6d385a1ced
Jake Sanghavi <jake.sanghavi@gmail.com>
2023-06-25 13:31:55 -0400

    Branch with useEffect code

--- frontend/src/components/Player.js
+++ frontend/src/components/Player.js
@@ -1,27 +1,59 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import '../styles.css';
 
 const Player = () => {
+  const widgetRef = useRef(null);
 
-    // useEffect(() => {
-    //     const iframe = document.getElementById('playbutton-iframe');
-    
-    //     const onLoadHandler = () => {
-    //       // Execute any communication with the iframe after it has fully loaded
-    //       // For example, you can send a message to the iframe using postMessage
-    //     };
-    
-    //     iframe.addEventListener('load', onLoadHandler);
-    
-    //     return () => {
-    //       iframe.removeEventListener('load', onLoadHandler);
-    //     };
-    //   }, []);
+  useEffect(() => {
+    if (!window.SC) {
+      const script = document.createElement('script');
+      script.src = 'https://w.soundcloud.com/player/api.js';
+      script.async = true;
+      script.onload = initializeWidget;
+      document.body.appendChild(script);
+    } else {
+      initializeWidget();
+    }
 
-    return (
-        <div className='player'>
-            <iframe id="playbutton-iframe" scrolling="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/255766429&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false"></iframe>
-        </div>
-    )
+    function initializeWidget() {
+      if (!widgetRef.current) {
+        widgetRef.current = window.SC.Widget(document.getElementById('iFrame'));
+
+        widgetRef.current.bind(window.SC.Widget.Events.READY, () => {
+
+          // Example: Bind an event listener to track finish
+          widgetRef.current.bind(window.SC.Widget.Events.FINISH, () => {
+            console.log('Track finished');
+          });
+
+          // Example: Seek to a specific position (in milliseconds)
+          const seekToPosition = 30000; // 30 seconds
+          widgetRef.current.seekTo(seekToPosition);
+        });
+      }
+    }
+  }, []);
+
+  function playSong() {
+    if (widgetRef.current) {
+      widgetRef.current.play();
+    }
+  }
+
+  return (
+    <div className='player'>
+      <button onClick={playSong}>PLAY</button>
+      <iframe
+        width="100%"
+        height="200"
+        style={{ display: 'none' }}
+        id="iFrame"
+        title="player"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/983989414&color=%23050301&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false"
+      ></iframe>
+    </div>
+  );
 }
 
-export default Player
+export default Player;
