7567cfb8005b93af887f4b2376971c190eb7308e
Jake Sanghavi <jake.sanghavi@gmail.com>
2023-07-14 09:29:35 -0400

    Changed CORS for live app

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use((request, response, next) => {
 })
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://musicle-official.netlify.app/',
     optionsSuccessStatus: 200
 }))
 
