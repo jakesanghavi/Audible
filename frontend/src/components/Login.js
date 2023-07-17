30b55c1febf2620d9d2bfc8fbae87a3dd3450f35
Jake Sanghavi <jake.sanghavi@gmail.com>
2023-07-17 18:47:28 -0400

    Added login functionality from DB

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -8,6 +8,8 @@ const Login = () => {
   const signUpEmail = useRef(null);
   const firstPassword = useRef(null);
   const confirmPassword = useRef(null);
+  const loginEmail = useRef(null);
+  const loginPassword = useRef(null);
 
   const signUpSelect = () => {
     setIsLoginSelected(false);
@@ -19,22 +21,75 @@ const Login = () => {
     loginForm.style.marginLeft = "0%";
   };
 
-  const checkUserExists = async () => {
+  const checkLogin= async() => {
+    const email_address = loginEmail.current.value;
+    const password = loginPassword.current.value;
+
+    if (email_address === '' | !email_address | password === '' | !password) {
+      if (email_address === '' | !email_address) {
+        console.log("Please input your email address.")
+      }
+      if (password === '' | !password) {
+        console.log("Please input your password.")
+      }
+      return;
+    }
+
+    try {
+      //dev
+      const response = await fetch('http://localhost:3008/api/users/' + email_address);
+      // const response = await fetch('https://musicle-official.onrender.com/api/songs/random/random');
+      console.log(response)
+      if (response.status === 399) {
+        console.log("User does not exist!")
+      }
+      else {
+        // dev
+        const resp = await fetch('http://localhost:3008/api/users/' + email_address);
+        const respJson = await resp.json();
+        if (respJson.password !== password) {
+          console.log("Password is incorrect.")
+        }
+        else {
+          console.log("Logged in!")
+        }
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  const checkSignup = async() => {
     const email_address = signUpEmail.current.value;
     const password = firstPassword.current.value;
     const passwordConfirm = confirmPassword.current.value;
+
+    if (email_address === '' | !email_address | password === '' | !password | passwordConfirm === '' | !passwordConfirm) {
+      if (email_address === '' | !email_address) {
+        console.log("Please input your email address.")
+      }
+      if (password === '' | !password) {
+        console.log("Please input your password.")
+      }
+      if (passwordConfirm === '' | !passwordConfirm) {
+        console.log("Please confirm password.")
+      }
+      return;
+    }
 
     if (password !== passwordConfirm) {
       console.log("Passwords must match!")
       return;
     }
 
-    //dev
     try {
+      //dev
       const response = await fetch('http://localhost:3008/api/users/' + email_address);
       // const response = await fetch('https://musicle-official.onrender.com/api/songs/random/random');
       console.log(response)
-      if (response.status == 200) {
+      if (response.status === 200) {
         console.log("Email Address already in use!")
       }
       else {
@@ -88,22 +143,22 @@ const Login = () => {
           <div className="form-inner">
             <div className="login">
               <div className="field">
-                <input type="text" placeholder="Email Address" required />
+                <input type="text" id="loginEmail" placeholder="Email Address" required ref={loginEmail}/>
               </div>
               <div className="field">
-                <input type="password" placeholder="Password" required />
+                <input type="password" id="loginPassword" placeholder="Password" required ref={loginPassword}/>
               </div>
               <div className="pass-link">
                 <a href="/">Forgot password?</a>
               </div>
               <div className="field btn">
                 <div className="btn-layer"></div>
-                <input style={{ width: "100%" }} type="button" value="Login" />
+                <input style={{ width: "100%" }} type="button" value="Login" onClick={checkLogin} />
               </div>
             </div>
             <div className="signup">
               <div className="field">
-                <input type="text" id="signUpUserName" placeholder="Email Address" required ref={signUpEmail} />
+                <input type="text" id="signUpEmail" placeholder="Email Address" required ref={signUpEmail} />
               </div>
               <div className="field">
                 <input type="password" id="signUpPassword" placeholder="Password" required ref={firstPassword} />
@@ -114,7 +169,7 @@ const Login = () => {
               </div>
               <div className="field btn">
                 <div className="btn-layer"></div>
-                <input type="button" style={{ width: "100%" }} value="Sign Up" onClick={checkUserExists} />
+                <input type="button" style={{ width: "100%" }} value="Sign Up" onClick={checkSignup} />
               </div>
             </div>
           </div>
